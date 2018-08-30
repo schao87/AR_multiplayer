9aa6ffe15ad0a73670cc21314f483512ba6f84a4
sunny.chao <sunny.chao@deutschinc.com>
2018-08-30 11:56:42 -0400

    made ammo

---
+++ AR_multiplayer/Assets/Scripts/BulletMoveScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletMoveScript : MonoBehaviour {
+    public int speed;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        gameObject.transform.Translate(Vector3.forward * Time.deltaTime * speed);
+	}
+}
--- AR_multiplayer/Assets/Scripts/TankShootingScript.cs
+++ AR_multiplayer/Assets/Scripts/TankShootingScript.cs
@@ -6,14 +6,15 @@ public class TankShootingScript : MonoBehaviour
 {
 
     PhotonView view;
-    public GameObject Bullet;
+
     public int force = 30;
-   
+    public GameObject AmmoPrefab = null;
+
     // Use this for initialization
     void Start()
     {
         view = GetComponent<PhotonView>();
-       ;
+       
     }
 
     // Update is called once per frame
@@ -23,14 +24,24 @@ public class TankShootingScript : MonoBehaviour
         //view rpc (remote procedure call) allows us to send a method to all clients connected
         // shootBullet is the method
         // photonTargets.all sends it to all clients
+
         if (view.isMine && Input.GetKeyDown(KeyCode.Space))
         {
-            view.RPC("ShootBullet", PhotonTargets.All, transform.Find("ShootPosition").transform.position, transform.Find("ShootPosition").transform.rotation);
+            //view.RPC("ShootBullet", PhotonTargets.All, transform.Find("ShootPosition").transform.position, transform.Find("ShootPosition").transform.rotation);
+            view.RPC("ShootBullet", PhotonTargets.All);
         }
 
+
+
     }
-    [PunRPC] void ShootBullet(Vector3 Pos, Quaternion quaat){
-        GameObject GO = Instantiate(Bullet, Pos, quaat) as GameObject;
-        GO.GetComponent<Rigidbody>().velocity = transform.TransformDirection(Vector3.forward * force);
+    //[PunRPC] void ShootBullet(Vector3 Pos, Quaternion quaat){
+    //    GameObject GO = Instantiate(Bullet, Pos, quaat) as GameObject;
+    //    GO.GetComponent<Rigidbody>().velocity = transform.TransformDirection(Vector3.forward * force);
+    //}
+
+    [PunRPC]void ShootBullet()
+    {
+        Instantiate(AmmoPrefab, transform.position, transform.rotation);
+      
     }
 }
