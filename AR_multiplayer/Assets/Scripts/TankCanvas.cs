444649d2139ca959343e32ed2d4a95990a217f5f
sunny.chao <sunny.chao@deutschinc.com>
2018-09-05 15:02:10 -0400

    player name canvas added.

---
+++ AR_multiplayer/Assets/Scripts/TankCanvas.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TankCanvas : MonoBehaviour {
+    public Text playerName;
+    PhotonView view;
+	// Use this for initialization
+	void OnEnable () {
+        view = GetComponent<PhotonView>();
+        if (view.isMine)
+        {
+            playerName.text = "Tank 1";
+        }
+        else
+        {
+            playerName.text = "Enemy";
+        }
+
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+       
+
+        Camera cam = Camera.main;
+        //makes canvas face main camera
+        transform.LookAt(transform.position + cam.transform.rotation * Vector3.forward, cam.transform.rotation * Vector3.up);
+    }
+}
