6d09242d03a7fb5275d031a413563408054d300d
sunny.chao <sunny.chao@deutschinc.com>
2018-09-10 18:36:57 -0400

    removed feathered plane when game starts

--- AR_multiplayer/Assets/Scripts/PlaceMapScript.cs
+++ AR_multiplayer/Assets/Scripts/PlaceMapScript.cs
@@ -5,14 +5,17 @@ using UnityEngine.UI;
 using UnityEngine.XR.ARFoundation;
 using UnityEngine.Experimental.XR;
 
-public class PlaceMapScript : MonoBehaviour {
+public class PlaceMapScript : MonoBehaviour
+{
     public GameObject MultiplayerBtn;
     public GameObject PlaceMapBtn;
     public GameObject ResetMapBtn;
     public GameObject ARSessionOrigin;
-    public GameObject FeatheredPlane;
-	// Use this for initialization
-    public void OnClickPlaceMapBtn(){
+    //public GameObject FeatheredPlane;
+    // Use this for initialization
+
+    public void OnClickPlaceMapBtn()
+    {
         //var SessionOrigin = GameObject.FindGameObjectsWithTag("ARSessionOrigin");
 
         ARSessionOrigin.GetComponent<ARPlaneManager>().enabled = false;
@@ -21,16 +24,33 @@ public class PlaceMapScript : MonoBehaviour {
         MultiplayerBtn.SetActive(true);
         PlaceMapBtn.SetActive(false);
         ResetMapBtn.SetActive(true);
-        FeatheredPlane.SetActive(false);
+
+        if (!Application.isEditor)
+        {
+            var FeatheredPlane = GameObject.FindGameObjectWithTag("FeatheredPlane");
+            var PointCloud = GameObject.FindGameObjectWithTag("PointCloud");
+
+            FeatheredPlane.SetActive(false);
+            PointCloud.SetActive(false);
+        }
     }
 
-    public void OnClickResetMap(){
+    public void OnClickResetMap()
+    {
         PlaceMapBtn.SetActive(true);
         ResetMapBtn.SetActive(false);
+       
         ARSessionOrigin.GetComponent<ARPlaneManager>().enabled = true;
         ARSessionOrigin.GetComponent<AutoPlaceItem>().enabled = true;
         ARSessionOrigin.GetComponent<ARPointCloudManager>().enabled = true;
-        FeatheredPlane.SetActive(true);
-    }
 
+        if (!Application.isEditor)
+        {
+            var FeatheredPlane = GameObject.FindGameObjectWithTag("FeatheredPlane");
+            var PointCloud = GameObject.FindGameObjectWithTag("PointCloud");
+
+            FeatheredPlane.SetActive(true);
+            PointCloud.SetActive(true);
+        }
+    }
 }
